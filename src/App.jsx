64f0e220d16b51c20b0959e28522c85c871e75c3
Andre Miras <andre.miras@gmail.com>
2020-11-23 17:07:54 +0100

    :construction: Adds Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Footer />
     </div>
   );
 }
