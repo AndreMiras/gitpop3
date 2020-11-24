20e74dcc64c607c130a063ad169972b5d5cac0f1
Andre Miras <andre.miras@gmail.com>
2020-11-24 06:17:49 +0100

    :wheelchair: Enter to submit the search

--- src/components/PopForm.jsx
+++ src/components/PopForm.jsx
@@ -1,22 +1,30 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, FormControl, InputGroup } from 'react-bootstrap';
+import {
+  Button, Form, FormControl, InputGroup,
+} from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PopForm = ({ onSubmit }) => {
   const [url, setUrl] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(url);
+  };
   return (
-    <InputGroup className="mb-3">
-      <FormControl
-        placeholder="https://github.com/django/django"
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <InputGroup.Append>
-        <Button variant="outline-secondary" onClick={() => onSubmit(url)}>
-          <FontAwesomeIcon icon="search" />
-        </Button>
-      </InputGroup.Append>
-    </InputGroup>
+    <Form onSubmit={handleSubmit}>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="https://github.com/django/django"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <InputGroup.Append>
+          <Button type="submit" variant="outline-secondary" onClick={() => onSubmit(url)}>
+            <FontAwesomeIcon icon="search" />
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </Form>
   );
 };
 PopForm.propTypes = {
