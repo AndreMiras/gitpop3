89607f906260116bfbc9473751a6ee1d3ef15a67
Andre Miras <andre.miras@gmail.com>
2020-12-08 09:48:59 +0100

    :recycle: Moves timeSince helper to dedicated file

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -3,34 +3,7 @@ import PropTypes from 'prop-types';
 import { Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Pagination from 'react-js-pagination';
-
-/**
- * https://stackoverflow.com/a/3177838
- */
-const timeSince = (date) => {
-  const seconds = Math.floor((new Date() - date) / 1000);
-  let interval = seconds / 31536000;
-  if (interval > 1) {
-    return `${Math.floor(interval)} years ago`;
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return `${Math.floor(interval)} months ago`;
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return `${Math.floor(interval)} days ago`;
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return `${Math.floor(interval)} hours ago`;
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return `${Math.floor(interval)} minutes ago`;
-  }
-  return `${Math.floor(seconds)} seconds ago`;
-};
+import timeSince from '../utils/time';
 
 const RepoLink = ({ nameWithOwner }) => (
   <a href={`https://github.com/${nameWithOwner}`}>
