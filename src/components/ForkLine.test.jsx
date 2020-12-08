cfa97d19f8d01b4d28d6752dec93ba980cc6d029
Andre Miras <andre.miras@gmail.com>
2020-12-08 19:06:07 +0100

    :white_check_mark: Tests ForkLine

---
+++ src/components/ForkLine.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import ForkLine from './ForkLine';
+
+test('renders', () => {
+  const info = {
+    nameWithOwner: 'django/django',
+    stargazerCount: 123,
+    forkCount: 321,
+    pushedAt: '2020-08-29T14:23:26Z',
+    object: {
+      history: {
+        totalCount: 1234,
+      },
+    },
+  };
+  const tree = renderer.create(
+    <ForkLine info={info} />,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
