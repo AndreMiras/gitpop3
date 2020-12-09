efaeed7ad7dd596a5ae527edc3faf976d76b81ea
Andre Miras <andre.miras@gmail.com>
2020-12-09 23:30:03 +0100

    :white_check_mark: Test PopForm.jsx submit

--- src/components/PopForm.test.jsx
+++ src/components/PopForm.test.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import PopForm from './PopForm';
@@ -25,4 +26,25 @@ describe.each([
     ).toJSON();
     expect(tree).toMatchSnapshot();
   });
+});
+
+test('submit', () => {
+  const onSubmit = jest.fn();
+  render(<PopForm onSubmit={onSubmit} loading={false} />);
+  const searchInput = screen.getByPlaceholderText(/github.com/);
+  const expectedUrl = 'https://github.com/django/django';
+  fireEvent.change(searchInput, { target: { value: expectedUrl } });
+  const submitButton = screen.getByRole('button', { type: 'submit' });
+  // mouse click
+  fireEvent.click(submitButton);
+  expect(onSubmit).toHaveBeenNthCalledWith(1, expectedUrl);
+  // invalid URL should not trigger the submit callback
+  const invalidUrl = 'https://github.com/invalid-url';
+  fireEvent.change(searchInput, { target: { value: invalidUrl } });
+  fireEvent.click(submitButton);
+  expect(onSubmit).toHaveBeenNthCalledWith(1, expectedUrl);
+  // submit (keyboard Enter) should also be mapped
+  fireEvent.change(searchInput, { target: { value: expectedUrl } });
+  fireEvent.submit(searchInput);
+  expect(onSubmit).toHaveBeenNthCalledWith(2, expectedUrl);
 });
