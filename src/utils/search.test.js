2069a901d8aa5c17b975d2be418cdf53a09c865d
Andre Miras <andre.miras@gmail.com>
2020-12-14 03:39:41 +0100

    :white_check_mark: Unit tests search error, refs #6

--- src/utils/search.test.js
+++ src/utils/search.test.js
@@ -1,3 +1,4 @@
+import { ApolloError } from '@apollo/client';
 import searchPopularForks from './search';
 import { client } from './graphql';
 
@@ -16,13 +17,36 @@ test('basic case', (done) => {
     expect(result).toEqual(expected);
     done();
   };
+  // test should fail onError
+  const onError = (error) => done(error);
   const querySpy = jest.spyOn(client, 'query').mockReturnValue(Promise.resolve(expected));
-  expect(
-    searchPopularForks(
-      url, onResult,
-    ),
-  );
+  searchPopularForks(url, onResult, onError);
   expect(querySpy).toHaveBeenNthCalledWith(
     1, { query: expect.any(Object), variables: { owner: 'django', name: 'django' } },
   );
+  querySpy.mockRestore();
+});
+
+test('onError', (done) => {
+  const url = 'https://github.com/django/django-404';
+  const expected = "Could not resolve to a Repository with the name 'django/django-404'.";
+  // test should fail onResult
+  const onResult = (result) => done(result);
+  const onError = (error) => {
+    expect(error).toEqual(new ApolloError({ errorMessage: expected }));
+    expect(error.message).toEqual(expected);
+    done();
+  };
+  const querySpy = jest.spyOn(
+    client, 'query',
+  ).mockReturnValue(
+    Promise.reject(
+      new ApolloError({ errorMessage: expected }),
+    ),
+  );
+  searchPopularForks(url, onResult, onError);
+  expect(querySpy).toHaveBeenNthCalledWith(
+    1, { query: expect.any(Object), variables: { owner: 'django', name: 'django-404' } },
+  );
+  querySpy.mockRestore();
 });
