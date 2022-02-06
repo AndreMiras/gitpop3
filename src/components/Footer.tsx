569ab77177d5907fd0a4e6cf0245eee30f5e43cd
Andre Miras <andre.miras@gmail.com>
2022-02-06 22:29:39 +0100

    :recycle: refactor: Convert Footer to TypeScript

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import { FunctionComponent } from "react";
 import { Container } from "react-bootstrap";
 import { version } from "../../package.json";
 
-const Footer = () => (
+const Footer: FunctionComponent = () => (
   <footer className="footer d-none d-md-block">
     <Container className="text-center">
       <span>Copyright &copy; Andre Miras 2020 - gitpop3 v{version}</span>
